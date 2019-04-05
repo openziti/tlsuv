43144fcbbbcf9e5ec881618126200feecbf6c200
Shawn Carey <shawn.carey@netfoundry.io>
2019-04-05 12:08:14 -0400

    rename queue macros when building on macos

--- src/bio.h
+++ src/bio.h
@@ -7,6 +7,16 @@
 
 #include <sys/queue.h>
 
+#if defined(__APPLE__)
+#define SIMPLEQ_INIT        STAILQ_INIT
+#define SIMPLEQ_HEAD        STAILQ_HEAD
+#define SIMPLEQ_ENTRY       STAILQ_ENTRY
+#define SIMPLEQ_EMPTY       STAILQ_EMPTY
+#define SIMPLEQ_FIRST       STAILQ_FIRST
+#define SIMPLEQ_INSERT_TAIL STAILQ_INSERT_TAIL
+#define SIMPLEQ_REMOVE_HEAD STAILQ_REMOVE_HEAD
+#endif
+
 typedef struct bio {
     size_t available;
     size_t headoffset;
