6b205957860e53b93764e8aab3d8297379d9fb60
Shawn Carey <shawn.carey@netfoundry.io>
2019-04-10 13:59:24 -0400

    remove macos compatibility macros. use STAILQ macros from sys/queue.h instead of SIMPLEQ.

--- src/bio.c
+++ src/bio.c
@@ -12,21 +12,21 @@ struct msg {
     size_t len;
     uint8_t *buf;
 
-    SIMPLEQ_ENTRY(msg) next;
+    STAILQ_ENTRY(msg) next;
 };
 
 BIO *BIO_new() {
     BIO* bio = calloc(1, sizeof(BIO));
     bio->available = 0;
     bio->headoffset = 0;
-    SIMPLEQ_INIT(&bio->message_q);
+    STAILQ_INIT(&bio->message_q);
     return bio;
 }
 
 void BIO_free(BIO* b) {
-    while(!SIMPLEQ_EMPTY(&b->message_q)) {
-        struct msg *m = SIMPLEQ_FIRST(&b->message_q);
-        SIMPLEQ_REMOVE_HEAD(&b->message_q, next);
+    while(!STAILQ_EMPTY(&b->message_q)) {
+        struct msg *m = STAILQ_FIRST(&b->message_q);
+        STAILQ_REMOVE_HEAD(&b->message_q, next);
         free(m->buf);
         free(m);
     }
@@ -45,15 +45,15 @@ void BIO_put(BIO *bio, const uint8_t *buf, size_t len) {
     memcpy(m->buf, buf, len);
     bio->available += len;
 
-    SIMPLEQ_INSERT_TAIL(&bio->message_q, m, next);
+    STAILQ_INSERT_TAIL(&bio->message_q, m, next);
 }
 
 int BIO_read(BIO *bio, uint8_t *buf, size_t len) {
-    if (SIMPLEQ_EMPTY(&bio->message_q)) {
+    if (STAILQ_EMPTY(&bio->message_q)) {
         return -1;
     }
 
-    struct msg *m = SIMPLEQ_FIRST(&bio->message_q);
+    struct msg *m = STAILQ_FIRST(&bio->message_q);
 
     size_t recv_size = MIN(len, m->len - bio->headoffset);
     memcpy(buf, m->buf + bio->headoffset, recv_size);
@@ -61,7 +61,7 @@ int BIO_read(BIO *bio, uint8_t *buf, size_t len) {
     bio->available -= recv_size;
 
     if (bio->headoffset == m->len) {
-        SIMPLEQ_REMOVE_HEAD(&bio->message_q, next);
+        STAILQ_REMOVE_HEAD(&bio->message_q, next);
         bio->headoffset = 0;
 
         free(m->buf);
@@ -70,5 +70,3 @@ int BIO_read(BIO *bio, uint8_t *buf, size_t len) {
 
     return (int) recv_size;
 }
-
-
