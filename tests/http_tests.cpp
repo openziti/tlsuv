c7321fd131dc6a0fe6585b44eaca8013d1c885ad
Eugene K <eugene.kobyakov@netfoundry.io>
2020-06-15 10:30:52 -0400

    http headers are case insensitive

--- tests/http_tests.cpp
+++ tests/http_tests.cpp
@@ -25,6 +25,23 @@ limitations under the License.
 using namespace std;
 using namespace Catch::Matchers;
 
+struct ci_less : std::binary_function<string, string, bool>
+{
+    // case-independent (ci) compare_less binary function
+    struct nocase_compare : public std::binary_function<unsigned char,unsigned char,bool>
+    {
+      bool operator() (const unsigned char& c1, const unsigned char& c2) const {
+          return tolower (c1) < tolower (c2);
+      }
+    };
+    bool operator() (const std::string & s1, const std::string & s2) const {
+      return std::lexicographical_compare
+        (s1.begin (), s1.end (),   // source range
+        s2.begin (), s2.end (),   // dest range
+        nocase_compare ());  // comparison
+    }
+};
+
 class resp_capture {
 public:
 
@@ -37,7 +54,7 @@ public:
     string http_version;
     ssize_t code{};
     string status;
-    map<string, string> headers;
+    map<string, string, ci_less> headers;
 
     string body;
     string req_body;
