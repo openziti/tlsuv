e91a0b3eb88c71b7266feda0a1af35a56c43d8f5
Shawn Carey <shawn.carey@netfoundry.io>
2022-06-21 10:00:14 -0400

    get libuv 1.44.1

--- deps/libuv.cmake
+++ deps/libuv.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(libuv
         GIT_REPOSITORY https://github.com/libuv/libuv.git
-        GIT_TAG v1.34.2
+        GIT_TAG v1.44.1
         GIT_SHALLOW 1
 )
 
