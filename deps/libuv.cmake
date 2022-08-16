d8d2cfd77d9bcb58a6c5b7b0c4cf1873a351a420
Shawn Carey <shawn.carey@netfoundry.io>
2022-08-16 13:45:27 -0400

    get libuv 1.44.1 (#113)
    * get libuv 1.44.1
    
    * allow downstream projects override libuv version if needed
    
    * update CI env (macos-11)
    
    Co-authored-by: Eugene K <eugene.kobyakov@netfoundry.io>

--- deps/libuv.cmake
+++ deps/libuv.cmake
@@ -1,7 +1,14 @@
 include(FetchContent)
+
+if (NOT DEFINED LIBUV_VERSION)
+    set(LIBUV_VERSION v1.44.1)
+endif()
+
+message("using libuv@${LIBUV_VERSION}")
+
 FetchContent_Declare(libuv
         GIT_REPOSITORY https://github.com/libuv/libuv.git
-        GIT_TAG v1.34.2
+        GIT_TAG ${LIBUV_VERSION}
         GIT_SHALLOW 1
 )
 
