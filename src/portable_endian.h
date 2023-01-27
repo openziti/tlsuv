fa4d17a4db541d52b0afd41de00e1baaa053695f
Eugene K <eugene.kobyakov@netfoundry.io>
2023-01-27 09:01:54 -0500

    update copyright notices

--- src/portable_endian.h
+++ src/portable_endian.h
@@ -1,3 +1,17 @@
+// Copyright (c) NetFoundry Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // "License": Public Domain
 // I, Mathias Panzenb�ck, place this file hereby into the public domain. Use it at your own risk for whatever you like.
 // In case there are jurisdictions that don't support putting things in the public domain you can also consider it to
