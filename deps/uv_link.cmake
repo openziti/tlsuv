b8f164cbd987934aa788e61a1fc909a6b013f9a6
ekoby <7406535+ekoby@users.noreply.github.com>
2023-02-08 14:29:31 -0500

    Better builds 2 (#129)
    * refactor dependencies: zlib, libuv

--- deps/uv_link.cmake
+++ deps/uv_link.cmake
@@ -1,4 +1,3 @@
-message("current dir = ${CMAKE_CURRENT_SOURCE_DIR}")
 set(uvl_src ${CMAKE_CURRENT_SOURCE_DIR}/uv_link_t)
 add_library(uv_link OBJECT
         ${uvl_src}/src/uv_link_t.c
@@ -11,6 +10,4 @@ target_include_directories(uv_link
         PRIVATE ${uvl_src}
 )
 
-if(NOT HAVE_LIBUV)
-    target_include_directories(uv_link PRIVATE ${libuv_SOURCE_DIR}/include)
-endif()
+target_link_libraries(uv_link ${TLSUV_LIBUV_LIB})
