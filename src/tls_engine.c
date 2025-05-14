f46dee1f0436d1a4a129e24f6bf9c30775a2a99e
ekoby <eugene.kobyakov@netfoundry.io>
2025-05-14 15:38:49 -0400

    add missing include

--- src/tls_engine.c
+++ src/tls_engine.c
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <string.h>
 #include <tlsuv/tls_engine.h>
 #include "um_debug.h"
 
